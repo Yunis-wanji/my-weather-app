2e2ed0a295d830a94bf683d2ba4087a67229b860
Yunis-wanji <94746081+Yunis-wanji@users.noreply.github.com>
2022-01-06 12:09:17 +0300

    Changed my files

---
+++ src/index.js
@@ -0,0 +1,57 @@
+let now = new Date();
+
+let date = now.getDate();
+let year = now.getFullYear();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"];
+let day = days[now.getDay()];
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+let month = months[now.getMonth()];
+let hour = now.getHours();
+let min = now.getMinutes();
+let today = document.querySelector("p");
+today.innerHTML = `${day} ${date} ${month} ${hour}:${min} ${year}`;
+
+function displayWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humid").innerHTML = Math.round(
+    response.data.main.humidity
+  );
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#describe").innerHTML = response.data.weather[0].main;
+}
+function search(city) {
+  let apiKey = "e22adb13ecfe9db5af2e8b64dfc0ed33";
+  let apiUrl = `https:/api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function submitWeather(event) {
+  event.preventDefault();
+  // let searchInput = document.querySelector("#search-text-input");
+  // let h1 = document.querySelector("h1");
+  // h1.innerHTML = searchInput.value;
+  let city = document.querySelector("#search-text-input").value;
+  search(city);
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", submitWeather);
+
+search("New York");
